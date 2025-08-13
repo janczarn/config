051448b5ab8839a681bb6cf1ac0f0ee793634a7f
Jan Czarnecki <jmc254@cam.ac.uk>
2025-08-13 13:48:18 +0100

    Open files with spaces in name.

--- yazi/.config/yazi/signal/edit_in_helix.sh
+++ yazi/.config/yazi/signal/edit_in_helix.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 zellij action go-to-tab-name "helix"
 zellij action write 27 # send <Escape> key
-zellij action write-chars ":open $1"
+zellij action write-chars ":open \"$1\""
 zellij action write 13 # send <Enter> key
